cc9eeebf1b832beeacc27d13671cbaf69bc79a9b
typeYu <mytypeyu@gmail.com>
2026-01-23 14:26:48 +0900

    fix: home rollback #5

--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,3 @@
-'use client';
-
-import { useState } from 'react';
-
-import { AgreeTerms } from '@/components/cmm/AgreeTerms';
-
 export default function Home() {
-  const [base, setBase] = useState(false);
-  const [pension, setPension] = useState(false);
-  const [fund, setFund] = useState(false);
-
-  return (
-    <AgreeTerms
-      label="전체 동의"
-      items={[
-        {
-          content: '금융거래 기본약관',
-          checked: base,
-          onCheckedChange: setBase,
-        },
-        {
-          content: '연금저축 약관',
-          checked: pension,
-          onCheckedChange: setPension,
-        },
-        {
-          content: '집합투자규약',
-          checked: fund,
-          onCheckedChange: setFund,
-        },
-      ]}
-    />
-  );
+  return <div>home</div>;
 }
