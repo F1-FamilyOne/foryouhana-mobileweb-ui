bb7f8d700f51e76334678f16ab593aafbe179581
Jeongsu0880 <latsyrc900@gmail.com>
2026-01-26 18:46:31 +0900

    refactor: css 조정 #24

--- app/onboarding/edit/page.tsx
+++ app/onboarding/edit/page.tsx
@@ -105,44 +105,46 @@ export default function planEdit() {
     const nextDraft = buildDraftFromForm(form, draft);
 
     sessionStorage.setItem('giftPlan', JSON.stringify(nextDraft));
-
+    console.log(draft);
     router.back();
   };
   return (
-    <div>
+    <div className="flex flex-col">
       <Header content="플랜 직접 수정하기" />
-      <div className="grid w-92.25 gap-3">
-        <h1 className="font-hana-light">
-          원하는 대로 증여 계획을 조정해보세요.
-        </h1>
-        <div className="rounded-2xl border border-hana-gray-300 p-5 shadow-hana-gray-300 shadow-sm">
-          <PensionSection
-            select={form.isPension}
-            onSelect={(isSelected) => {
-              dispatch({ isPension: isSelected });
-            }}
-          />
-          <GiftSection
-            giftPlan={form.isGift}
-            onGiftPlanSelected={(isSelected) => {
-              dispatch({ isGift: isSelected, isRegular: isSelected });
-            }}
-            regular={form.isRegular}
-            onRegularSelected={(isSelected) => {
-              dispatch({ isRegular: isSelected });
-            }}
-            monthlyMoney={Number(draft?.plan.monthly_money)}
-            inMonth={Number(draft?.plan.in_month)}
-          />
+      <main className="flex-1">
+        <div className="grid w-92.25 gap-3">
+          <h1 className="font-hana-light">
+            원하는 대로 증여 계획을 조정해보세요.
+          </h1>
+          <div className="rounded-2xl border border-hana-gray-300 p-5 shadow-hana-gray-300 shadow-sm">
+            <PensionSection
+              select={form.isPension}
+              onSelect={(isSelected) => {
+                dispatch({ isPension: isSelected });
+              }}
+            />
+            <GiftSection
+              giftPlan={form.isGift}
+              onGiftPlanSelected={(isSelected) => {
+                dispatch({ isGift: isSelected, isRegular: isSelected });
+              }}
+              regular={form.isRegular}
+              onRegularSelected={(isSelected) => {
+                dispatch({ isRegular: isSelected });
+              }}
+              monthlyMoney={Number(draft?.plan.monthly_money)}
+              inMonth={Number(draft?.plan.in_month)}
+            />
+          </div>
         </div>
-        <div className="grid justify-center gap-2 pt-4">
-          <CustomButton preset="lightgraylong" onClick={router.back}>
-            돌아가기
-          </CustomButton>
-          <CustomButton preset="greenlong" onClick={handleSaveDraft}>
-            이 플랜으로 변경하기
-          </CustomButton>
-        </div>
+      </main>
+      <div className="grid justify-center gap-2 pt-4">
+        <CustomButton preset="lightgraylong" onClick={router.back}>
+          돌아가기
+        </CustomButton>
+        <CustomButton preset="greenlong" onClick={handleSaveDraft}>
+          이 플랜으로 변경하기
+        </CustomButton>
       </div>
     </div>
   );
